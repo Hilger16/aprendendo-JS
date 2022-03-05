9bdc2ab4a57251b6a6ab549fc65ef7ec1a0d088d
Hilger16 <97545429+Hilger16@users.noreply.github.com>
2022-03-05 10:46:27 -0300

    orientação à objetos JS

---
+++ js_assincrono/assets/js/scripts.js
@@ -0,0 +1,24 @@
+//quando for usar APIs se cria uma constante com a url para não precisar digitar toda vez a string com o caminho
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+const catImg = document.getElementById('cat');
+
+const getCats = async () => {
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+
+        return json.webpurl;
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+
+const loadImg = async () => {
+    const catImg = document.getElementById('cat');
+    catImg.src = await getCats();
+}
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
